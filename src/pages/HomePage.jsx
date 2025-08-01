f33e3102f399eb5497a699fbfbeff4bc266a6814
prince <princesuke@gmail.com>
2025-08-01 09:27:12 +0700

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-10 space-y-10">
+      <h1 className="text-2xl font-bold text-center">Animation</h1>
+    </div>
+  );
+}
